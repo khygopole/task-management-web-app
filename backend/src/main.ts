13180a87758d471cf39bff5088928880a292f288
Khyle Matthew Gopole <khylematthewgopole@gmail.com>
2025-08-09 17:22:09 +0800

    Testing production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,9 +8,26 @@ import UserRoutes from "./routes/User";
 import TaskRoutes from "./routes/Task";
 import { ConnectMongoAtlas } from "./database/database";
 
+// Staging
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 // Setup Express and Port
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+// __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static frontend
+app.use(express.static(path.join(__dirname, "public")));
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Set Cors so that server can connect to the client via port 5173
 app.use(
@@ -42,7 +59,7 @@ app.use("/users", UserRoutes);
 app.use("/tasks", TaskRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on PORT ${PORT}`);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`);
 });
 
 app.get("/", (res: Response) => {
